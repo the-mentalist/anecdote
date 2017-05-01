4fd12c262082fc495a404f3fbc5e75e531220536
Navpreet Ahuja <Navpreet Ahuja>
2017-05-02 01:33:28 +0530

    minor format changes

--- media.py
+++ media.py
@@ -3,23 +3,31 @@ import template
 
 redemption = movie.Movie(
     "The Shawshank Redemption",
-    "https://s-media-cache-ak0.pinimg.com/736x/93/fe/0f/93fe0ff5efd5812671803fb75a12e0b5.jpg",
+    "https://s-media-cache-ak0.pinimg.com/736x/93/fe/0f/"
+    "93fe0ff5efd5812671803fb75a12e0b5.jpg",
     "https://www.youtube.com/watch?v=6hB3S9bIaco",
-    "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "Two imprisoned men bond over a number of years, "
+    "finding solace and eventual redemption through acts of common decency.",
     )
 
 dark_song = movie.Movie(
     "A Dark Song",
-    "http://kaleidoscope-admin.piggy.co.uk/site/assets/files/1726/a_dark_song_one_sheet_approved.jpg",
+    "http://kaleidoscope-admin.piggy.co.uk/site/assets/files/1726/"
+    "a_dark_song_one_sheet_approved.jpg",
     "https://www.youtube.com/watch?v=vvQ2ClKbRcU",
-    "Drama: A determined young woman and a damaged occultist risk their lives and souls to perform a dangerous ritual that will grant them what they want."
+    "Drama: A determined young woman and a damaged occultist risk"
+    " their lives and souls to perform a dangerous ritual that will"
+    " grant them what they want."
     )
 
 gump = movie.Movie(
     "Forrest Gump",
-    "http://2.bp.blogspot.com/-i03dWHqsKuY/Ucw2YpUKlOI/AAAAAAAAAFQ/NqfUh7pHZZQ/s1456/forrest-gump-poster-1994-tom-hanks.png",
+    "http://2.bp.blogspot.com/-i03dWHqsKuY/Ucw2YpUKlOI/AAAAAAAAAFQ/"
+    "NqfUh7pHZZQ/s1456/forrest-gump-poster-1994-tom-hanks.png",
     "https://www.youtube.com/watch?v=uPIEn0M8su0",
-    "Comedy: While not intelligent, Forrest Gump has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him."
+    "Comedy: While not intelligent, Forrest Gump has accidentally "
+    "been present at many historic moments, but his true love, Jenny Curran, "
+    "eludes him."
     )
 
 la_land = movie.Movie(
@@ -31,20 +39,24 @@ la_land = movie.Movie(
 
 ali_nino = movie.Movie(
     "Ali and Nino",
-    "https://trailers.apple.com/trailers/ifcfilms/ali-and-nino/images/poster-large.jpg?lastmod=1",
+    "https://trailers.apple.com/trailers/ifcfilms/ali-and-nino/images/"
+    "poster-large.jpg?lastmod=1",
     "https://www.youtube.com/watch?v=m8-Imlxsqhc",
-    "Drama/Romance: Love story of a Muslim Azerbaijani boy and Christian Georgian girl in Baku from 1918 to 1920."
+    "Drama/Romance: Love story of a Muslim Azerbaijani boy and Christian "
+    "Georgian girl in Baku from 1918 to 1920."
     )
-    
+
 assassins = movie.Movie(
     "Assassins Creed",
-    "http://cdn2-www.comingsoon.net/assets/uploads/gallery/assassins-creed-movie/asscreedinternational.jpg",
+    "http://cdn2-www.comingsoon.net/assets/uploads/gallery/assassins-creed"
+    "-movie/asscreedinternational.jpg",
     "https://www.youtube.com/watch?v=4haJD6W136c",
-    "When Callum Lynch explores the memories of his ancestor Aguilar and gains the skills of a Master Assassin,"
-    +"he discovers he is a descendant of the secret Assassins society."
+    "When Callum Lynch explores the memories of his ancestor Aguilar and "
+    "gains the skills of a Master Assassin,"
+    "he discovers he is a descendant of the secret Assassins society."
     )
-    
+
 # list of movies
-movies=[redemption, dark_song, gump, la_land, ali_nino, assassins]
+movies = [redemption, dark_song, gump, la_land, ali_nino, assassins]
 # render template with list of movies
 template.open_movies_page(movies)
--- movie.py
+++ movie.py
@@ -1,22 +1,26 @@
 class Movie():
-    """ Consists of information related to Movie
-		Args:
-			title: Title of Movie
-			img: url of poster
-			trailer_url: url of trailer
-			storyline: Movie storyline
+    """Consists of information related to Movie
+        Args:
+            title: Title of Movie
+            img: url of poster
+            trailer_url: url of trailer
+            storyline: Movie storyline
     """
     def __init__(self, title, img, trailer_url, storyline):
         # Constructor
-        self.title=title
-        self.img=img
-        self.trailer_url=trailer_url
-        self.storyline=storyline
+        self.title = title
+        self.img = img
+        self.trailer_url = trailer_url
+        self.storyline = storyline
+
     def getTrailerUrl(self):
         return self.trailer_url
+
     def getStoryline(self):
         return self.storyline
+
     def getTitle(self):
         return self.title
+
     def getImg(self):
         return self.img
